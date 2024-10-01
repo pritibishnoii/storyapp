791a949055788854672df713ce0e99d61935a384
priti <priti12bishnoi@gmail.com>
2024-10-01 09:03:50 +0530

    **

---
+++ src/constants/data.js
@@ -0,0 +1,34 @@
+const filters = [
+  {
+    name: "All",
+    imageUrl:
+      "https://imgs.search.brave.com/yj-Hk2Qk5xzCElyoTAwG7LUjgfPVk9HQXZG7ic2L5Rc/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzIxLzlk/LzYzLzIxOWQ2MzBi/NjYzYWFlZWUzNWQx/NTdhMzdkODM3MzFm/LmpwZw",
+  },
+  {
+    name: "Food",
+    imageUrl:
+      "https://imgs.search.brave.com/dgFV5r6N48r8gNY1r_nQGq2C_6yoAKj1hwolHpceFSk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9kZWxpY2lvdXMt/cGFzdGEtZGlzaGdy/aWxsZWQtY2hpY2tl/bi1wZW5uZS1wYXN0/YWZyZXNoLWJhc2ls/dmlicmFudC1jb2xv/cnMtZm9vZC1pbWFn/ZS11c2UtYWlfMTMw/Mzk1My0yMjE3MS5q/cGc_c2l6ZT02MjYm/ZXh0PWpwZw",
+  },
+  {
+    name: "Fitness",
+    imageUrl:
+      "https://imgs.search.brave.com/D8VFGya45Z-iGBYm7NG8tgYK-vqtfvo3WgJGqbcflvk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by95/b3VuZy1maXRuZXNz/LW1hbi1zdHVkaW9f/NzUwMi01MDA4Lmpw/Zz9zaXplPTYyNiZl/eHQ9anBn",
+  },
+  {
+    name: "Travel",
+    imageUrl:
+      "https://imgs.search.brave.com/QEjghpi1JzC5-qnr5DoJI-s2snwsFAdXHeoCFm9kkU8/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTU1/NDM5MzE1L3Bob3Rv/L3Bhc3Nlbmdlci1h/aXJwbGFuZS1mbHlp/bmctYWJvdmUtY2xv/dWRzLWR1cmluZy1z/dW5zZXQuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPUxKV2Fk/YnMzQi1qU0dKQlZ5/OXMwZjhnWk1IaTJO/dldGWGEzVkoybEZj/TDA9",
+    },
+  {
+    name: "Movie",
+    imageUrl:
+      "https://imgs.search.brave.com/rOovn0ALTWTMoaep1r5I10de-JPBD-OV-FcO6m2_OfU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI3/MTUyMjYwMS9waG90/by9wb3AtY29ybi1h/bmQtb24tcmVkLWFy/bWNoYWlyLWNpbmVt/YS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9WHdReG1mckhi/LU93VjVvblBVVzVB/cEI0UmFHQks3cG9T/SXpaajRxX05fZz0",
+  },
+  {
+    name: "Education",
+    imageUrl:
+      "https://imgs.search.brave.com/MrER6hq7EVDtwjOl9tu61ZNvINOjXuX9XoL-rXfmjvw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTY3/NjIyMDIyNS9waG90/by9jaGlsZHJlbi1s/ZWFybmluZy1pbi1h/LXNjaG9vbC1jbGFz/c3Jvb20ud2VicD9h/PTEmYj0xJnM9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWd6T3pHZEgt/U2FHYTRsVU9nUWNR/MEtJS09qVC1WOUh6/NFNlY0Q5eEFCMTQ9",
+  },
+];
+
+export default filters;
